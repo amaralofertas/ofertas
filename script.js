dd302f6dac83ae8c5345f92b4b67373554d7b878
amaralofertas <cupomz3iro@gmail.com>
2025-09-19 18:46:50 -0300

    Atualizando script.js via n8n

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const cupons = [
   {
     "tipo": "shopee",
+    "codigo": "4PP0Z4P6",
+    "condicoes": "Novo Cupom Shopee ! R$ 20,00 OFF em R$ 60,00 Resgate aqui👇",
+    "link": "https://s.shopee.com.br/1qQSP9GiRR"
+  },
+  {
+    "tipo": "shopee",
     "codigo": "SH0IG29X8TR",
     "condicoes": "Novo Cupom Shopee ! 🏷 Cashback de 50%, até R$ 50,00 em Moedas de volta Resgate aqui👇",
     "link": "https://s.shopee.com.br/1qQSP9GiRR"

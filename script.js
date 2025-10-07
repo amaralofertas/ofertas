6123d46edbfc3ff7c5fdbe595c7c7ab34a623324
amaralofertas <cupomz3iro@gmail.com>
2025-10-07 14:04:20 -0300

    Atualizando script.js via n8n

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const cupons = [
   {
+    "tipo": "ml",
+    "codigo": "V3MQU3T3M1",
+    "condicoes": "Novo Cupom Mercado Livre 💛 20% 🚨 Cupom do mercado Livre 🏷 20% de Desconto compra mínima de R$79 limite de R$150 🔗 Válido em 90% do site Resgate aqui👇",
+    "link": "https://mercadolivre.com/sec/2Z9KCQk"
+  },
+  {
     "tipo": "shopee",
     "codigo": "9YN7O05VD",
     "condicoes": "Novo Cupom Shopee ! 🏷 R$ 10,00 OFF em R$ 40,00 Resgate aqui👇",

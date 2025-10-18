5342960a161fb1c551157373e75870b4a6d80e1e
amaralofertas <cupomz3iro@gmail.com>
2025-10-18 13:04:24 -0300

    Atualizando script.js via n8n

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const cupons = [
   {
     "tipo": "ml",
+    "codigo": "Nenhum código de cupom encontrado.",
+    "condicoes": "Novo Cupom Mercado Livre 10% OFF acima de R$30 limitado a R$150 em FTW Resgate aqui👇",
+    "link": "https://mercadolivre.com/sec/2Z9KCQk"
+  },
+  {
+    "tipo": "ml",
     "codigo": "NOVIDADEMELI",
     "condicoes": "Novo Cupom Mercado Livre 💛 15% 🚨 Cupom Mercado Livre 🏷 15% de Desconto com Limite de R$ 60 🔗 Em todos produtos Resgate aqui👇",
     "link": "https://mercadolivre.com/sec/2Z9KCQk"

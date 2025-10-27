1ca88ca06adfb4dfd823b997a12bdfb8a3213d12
amaralofertas <cupomz3iro@gmail.com>
2025-10-27 12:01:18 -0300

    Atualizando script.js via n8n

--- script.js
+++ script.js
@@ -22,6 +22,12 @@ const cupons = [
     "codigo": "4LAf0l6Rmt",
     "condicoes": "Novo Cupom Shopee ! R$ 100 OFF em R$ 899 Resgate aqui👇",
     "link": "https://s.shopee.com.br/1qQSP9GiRR"
+  },
+  {
+    "tipo": "ml",
+    "codigo": "CUPOMHOJE",
+    "condicoes": "Novo Cupom Mercado Livre 💛 25% 🚨 Cupom do mercado Livre 🏷 25% de Desconto com Limite de R$ 60 🔗 Em todos produtos Resgate aqui👇",
+    "link": "https://mercadolivre.com/sec/2Z9KCQk"
   }
 ];
 

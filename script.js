34c611fb5c5b033ea0a2e2be15a61cb5e2bc6f8d
amaralofertas <cupomz3iro@gmail.com>
2025-11-01 14:26:23 -0300

    Atualizando script.js via n8n

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-const cupons = [];
+const cupons = [
+  {
+    "tipo": "shopee",
+    "codigo": "MAIORDOANO50",
+    "condicoes": "Novo Cupom Shopee ! 🚨 Cupom Shopee 🏷 R$ 50,00 OFF em R$ 249,00 Resgate aqui👇",
+    "link": "https://s.shopee.com.br/1qQSP9GiRR"
+  }
+];
 
 window.mostrarCupons = function(tipo){
   const container = document.getElementById('container-cupom');

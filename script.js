6f8bf2cbc32fff5f3fc1477a3dbcc665f6b304b4
amaralofertas <cupomz3iro@gmail.com>
2025-12-12 18:02:11 -0300

    Atualizando script.js via n8n

--- script.js
+++ script.js
@@ -24,6 +24,12 @@ const cupons = [
     "link": "https://s.shopee.com.br/1qQSP9GiRR"
   },
   {
+    "tipo": "shopee",
+    "codigo": "30OFFNO1212AF",
+    "condicoes": "Novo Cupom Shopee ! 🚨 30% OFF (desconto máximo de R$15) 🏷 20% OFF (desconto máximo de R$5) Resgate aqui👇",
+    "link": "https://s.shopee.com.br/1qQSP9GiRR"
+  },
+  {
     "tipo": "ml",
     "codigo": "MELI12D12",
     "condicoes": "Novo Cupom Mercado Livre 🚨 Cupom do Mercado Livre 🏷 18% de Desconto com Limite de R$ 60 e 🏷 30% de Desconto com Limite de R$ 25 🔗 Em todos os produtos Resgate aqui👇",

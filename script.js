4c985d46490f75be594877f5f0048942c5f21f1f
cupomlivre <cupomz3iro@gmail.com>
2026-04-14 13:53:37 -0300

    Atualizando script.js via n8n

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-const cupons = [
-  {
-    "tipo": "ml",
-    "codigo": "RECEBIDOSPAGOS",
-    "condicoes": "Novo Cupom Mercado Livre 10% OFF em R$ 149, Limitado a R$ 60 OFF Resgate aqui👇",
-    "link": "https://mercadolivre.com/sec/2Z9KCQk"
-  }
-];
+const cupons = [];
 
 window.mostrarCupons = function(tipo){
   const container = document.getElementById('container-cupom');
